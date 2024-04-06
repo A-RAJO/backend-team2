b1bc163e46a2d2c6cc276360f844344114039ece
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-04-06 10:38:52 +0900

    first commit

---
+++ src/test/java/com/example/backendfirstprojectteam2/BackendFirstProjectTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendfirstprojectteam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFirstProjectTeam2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
